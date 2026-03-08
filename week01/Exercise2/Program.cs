173a2caccc1c9df17544bf94f0553d3f9882d646
chattkato <ckato@byupathway.edu>
2026-03-08 01:03:33 +0100

    ex 2 commit

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,23 +4,76 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter Number: ")
-        string valuefromuser = Console.ReadLine();
-
-        int x = int.Parse(valuefromuser);
-        int y = 2;
-
-        if (x > y)
+        // Get the grade from the user
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+        
+    
+        string gradeLetter = "";
+        
+        
+        if (percent >= 90)
         {
-            Console.WriteLine("Greater");
+            gradeLetter = "A";
         }
-        else if (x < y)
+        else if (percent >= 80)
         {
-            Console.WriteLine("Less");
-}
+            gradeLetter = "B";
+        }
+        else if (percent >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (percent >= 60)
+        {
+            gradeLetter = "D";
+        }
         else
         {
-            Console.WriteLine("Equal");
+            gradeLetter = "F";
         }
+        
+        
+        string plusMinus = "";
+        int remainder = percent % 10;
+        
+        
+        if (gradeLetter != "F")
+        {
+            if (remainder >= 7)
+            {
+                plusMinus = "+";
+            }
+            else if (remainder <= 2)
+            {
+                plusMinus = "-";
+            }
         }
+        
+        
+        if (gradeLetter == "A" && plusMinus == "+")
+        {
+            plusMinus = "";
         }
+        
+        
+        if (gradeLetter == "F")
+        {
+            plusMinus = "";
+        }
+        
+        
+        Console.WriteLine("Your grade is: " + gradeLetter + plusMinus);
+        
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Good job! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass this time. Keep trying!");
+        }
+    }
+}
