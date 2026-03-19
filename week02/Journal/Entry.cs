79f54a4dd8ba13baafb04c8fdda22e649414982a
chattkato <cdex2009@yahoo.com>
2026-03-19 20:05:37 +0100

    Update Entry.cs

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -2,17 +2,17 @@ using System;
 
 public class Entry
 {
-    public string _date;
-    public string _promptText;
-    public string _entryText;
-    public string _mood;
+    public string _Date;
+    public string _PromptText;
+    public string _EntryText;
+    public string _Mood;
 
     public void Display()
     {
-        Console.WriteLine($"Date: {_date}");
-        Console.WriteLine($"Prompt: {_promptText}");
-        Console.WriteLine($"Entry: {_entryText}");
-        Console.WriteLine($"Mood: {_mood}");
+        Console.WriteLine($"Date: {_Date}");
+        Console.WriteLine($"Prompt: {_PromptText}");
+        Console.WriteLine($"Entry: {_EntryText}");
+        Console.WriteLine($"Mood: {_Mood}");
         Console.WriteLine();
     }
 }
