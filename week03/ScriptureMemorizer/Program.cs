63dc742f2205f772817cb5a55c90d62fa6748636
chattkato <ckato@byupathway.edu>
2026-03-20 19:54:48 +0100

    scripture commit

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+
+        Scripture scripture = new Scripture(reference,
+        "Trust in the Lord with all thine heart and lean not unto thine own understanding in all thy ways acknowledge him and he shall direct thy paths");
+
+        string input = "";
+
+        while (input.ToLower() != "quit" && !scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to hide words or type quit to exit.");
+            input = Console.ReadLine();
+
+            if (input.ToLower() != "quit")
+            {
+                scripture.HideRandomWords(3);
+            }
+        }
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+        Console.WriteLine("\nAll words are now hidden. Program finished.");
     }
 }
