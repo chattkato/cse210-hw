05c36448ac8059c86697f5e09751b66016e3a82a
chattkato <ckato@byupathway.edu>
2026-03-22 14:47:48 +0100

    program commit

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -5,14 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Address address1 = new Address("123 Main St", "New York", "NY", "USA");
-        Customer customer1 = new Customer("John Doe", address1);
+        Customer customer1 = new Customer("Christiana Chatt", address1);
 
         Order order1 = new Order(customer1);
         order1.AddProduct(new Product("Laptop", "P100", 800, 1));
         order1.AddProduct(new Product("Mouse", "P101", 20, 2));
 
-        Address address2 = new Address("45 Abuja Road", "Abuja", "FCT", "Nigeria");
-        Customer customer2 = new Customer("Jane Smith", address2);
+        Address address2 = new Address("45 Nepa Road, Kubwa", "Abuja", "FCT", "Nigeria");
+        Customer customer2 = new Customer("Chatt Kato", address2);
 
         Order order2 = new Order(customer2);
         order2.AddProduct(new Product("Phone", "P200", 500, 1));
